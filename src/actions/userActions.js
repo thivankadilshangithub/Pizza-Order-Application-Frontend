85f928e6e290f684c66ab6b4758e32973c475b37
Thivanka Dilshan <86848030+thivankadilshangithub@users.noreply.github.com>
2022-06-01 17:48:47 +0530

    Update userActions.js

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -4,7 +4,7 @@ export const registerUser=(user)=>async dispatch=>{
     dispatch({type:'USER_REGISTER_REQUEST'})
 
     try {
-        const response = await axios.post('https://mern-pizzaorder-application.herokuapp.com/api/users/register' , user)
+        const response = await axios.post('/api/users/register' , user)
         console.log(response)
         dispatch({type:'USER_REGISTER_SUCCESS'})
         
@@ -19,7 +19,7 @@ export const loginUser=(user)=>async dispatch=>{
     dispatch({type:'USER_LOGIN_REQUEST'})
 
     try {
-        const response = await axios.post('https://mern-pizzaorder-application.herokuapp.com/api/users/login' , user)
+        const response = await axios.post('/api/users/login' , user)
         console.log(response)
         dispatch({type:'USER_LOGIN_SUCCESS' , payload : response.data})
         localStorage.setItem('currentUser' , JSON.stringify(response.data))
